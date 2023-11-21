6cb2423befca817fb21570204e73d88853aed81d
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-11-21 13:29:34 -0500

    devdeps update

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import auth from "./auth";
 import blogposts from "./blogposts";
@@ -13,8 +14,6 @@ const reducer = combineReducers({
 let store;
 
 if (process.env.NODE_ENV === `development`) {
-  import logger from "redux-logger";
-
   store = createStore(reducer, applyMiddleware(thunk, logger));
 } else {
   console.log("in", process.env.NODE_ENV);
