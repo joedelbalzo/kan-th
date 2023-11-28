cee6c5bb750ba1f2f91547ec6aa59340d4fad136
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-11-27 22:04:12 -0500

    node mem

--- backend/db/index.js
+++ backend/db/index.js
@@ -30,52 +30,51 @@ const syncAndSeed = async () => {
       }),
     ]);
 
-    const [samplePost1, samplePost2, samplePost3, samplePost4, samplePost5, samplePost6] =
-      await Promise.all([
-        Blogpost.create({
-          title: "Understanding Personal Finance: A Beginner's Guide",
-          subtitle: "Navigating the Basics of Money Management",
-          content: faker.lorem.paragraph(4),
-          published: false,
-          publishedAt: "Mon Jul 10 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-        Blogpost.create({
-          title: "Post 2. Smart Investment Strategies for Beginners",
-          subtitle: "Making Your Money Work for You",
-          content:
-            "<strong>This is blog post number 2!</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/><br/>",
-          published: true,
-          publishedAt: "Mon Aug 12 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-        Blogpost.create({
-          title: "Post 3. The Importance of Building an Emergency Fund",
-          subtitle: "Secure Your Financial Future with Smart Saving",
-          content: `This is blog post number 3! ${faker.lorem.paragraphs(5, "<br/></br>\n")}`,
-          published: true,
-          publishedAt: "Mon Nov 14 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-        Blogpost.create({
-          title: "Post 4. The Importance of Building an Emergency Fund",
-          subtitle: "Secure Your Financial Future with Smart Saving",
-          content: `This is blog post number 4! ${faker.lorem.paragraphs(5, "<br/></br></br>\n")}`,
-          published: true,
-          publishedAt: "Mon Nov 16 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-        Blogpost.create({
-          title: "Post 5. The Importance of Building an Emergency Fund",
-          subtitle: "Secure Your Financial Future with Smart Saving",
-          content: `This is blog post number 5! ${faker.lorem.paragraphs(5, "<br/></br>\n")}`,
-          published: true,
-          publishedAt: "Mon Nov 18 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-        Blogpost.create({
-          title: "Post 6. The Importance of Building an Emergency Fund",
-          subtitle: "Secure Your Financial Future with Smart Saving",
-          content: `This is blog post number 6! ${faker.lorem.paragraphs(5, "<br/></br>\n")}`,
-          published: true,
-          publishedAt: "Mon Nov 20 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
-        }),
-      ]);
+    const [samplePost1, samplePost2, samplePost3, samplePost4, samplePost5, samplePost6] = await Promise.all([
+      Blogpost.create({
+        title: "Understanding Personal Finance: A Beginner's Guide",
+        subtitle: "Navigating the Basics of Money Management",
+        content: faker.lorem.paragraph(3),
+        published: false,
+        publishedAt: "Mon Jul 10 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+      Blogpost.create({
+        title: "Post 2. Smart Investment Strategies for Beginners",
+        subtitle: "Making Your Money Work for You",
+        content:
+          "<strong>This is blog post number 2!</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/><br/>",
+        published: true,
+        publishedAt: "Mon Aug 12 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+      Blogpost.create({
+        title: "Post 3. The Importance of Building an Emergency Fund",
+        subtitle: "Secure Your Financial Future with Smart Saving",
+        content: `This is blog post number 3! ${faker.lorem.paragraphs(3, "<br/></br>\n")}`,
+        published: true,
+        publishedAt: "Mon Nov 14 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+      Blogpost.create({
+        title: "Post 4. The Importance of Building an Emergency Fund",
+        subtitle: "Secure Your Financial Future with Smart Saving",
+        content: `This is blog post number 4! ${faker.lorem.paragraphs(3, "<br/></br></br>\n")}`,
+        published: true,
+        publishedAt: "Mon Nov 16 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+      Blogpost.create({
+        title: "Post 5. The Importance of Building an Emergency Fund",
+        subtitle: "Secure Your Financial Future with Smart Saving",
+        content: `This is blog post number 5! ${faker.lorem.paragraphs(3, "<br/></br>\n")}`,
+        published: true,
+        publishedAt: "Mon Nov 18 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+      Blogpost.create({
+        title: "Post 6. The Importance of Building an Emergency Fund",
+        subtitle: "Secure Your Financial Future with Smart Saving",
+        content: `This is blog post number 6! ${faker.lorem.paragraphs(3, "<br/></br>\n")}`,
+        published: true,
+        publishedAt: "Mon Nov 20 2023 11:06:12 GMT-0500 (Eastern Standard Time)",
+      }),
+    ]);
 
     const [trials, errors, money, finances, smbs, dei] = await Promise.all([
       Tag.create({ tagName: "Trials" }),
@@ -86,57 +85,51 @@ const syncAndSeed = async () => {
       Tag.create({ tagName: "Diversity and Inclusion" }),
     ]);
 
-    const [
-      financeJpgHome,
-      financeDefinitionHome,
-      financeJpgHome2,
-      financeDefinitionHome2,
-      financeJpgHome3,
-      financeDefinitionHome3,
-    ] = await Promise.all([
-      Image.create({
-        position: "home",
-        awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
-        picNickname: "financejpg.jpg",
-        // awsPicURL,
-        picCaption: "home 1",
-      }),
-      Image.create({
-        position: "home",
-        awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
-        picNickname: "giphy.gif",
-        // awsPicURL,
-        picCaption: "home 1",
-      }),
-      Image.create({
-        position: "home",
-        awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
-        picNickname: "financejpg.jpg",
-        // awsPicURL,
-        picCaption: "home 2",
-      }),
-      Image.create({
-        position: "home",
-        awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
-        picNickname: "giphy.gif",
-        // awsPicURL,
-        picCaption: "home 2",
-      }),
-      Image.create({
-        position: "home",
-        awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
-        picNickname: "financejpg.jpg",
-        // awsPicURL,
-        picCaption: "home 3",
-      }),
-      Image.create({
-        position: "home",
-        awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
-        picNickname: "giphy.gif",
-        // awsPicURL,
-        picCaption: "home 3",
-      }),
-    ]);
+    const [financeJpgHome, financeDefinitionHome, financeJpgHome2, financeDefinitionHome2, financeJpgHome3, financeDefinitionHome3] =
+      await Promise.all([
+        Image.create({
+          position: "home",
+          awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
+          picNickname: "financejpg.jpg",
+          // awsPicURL,
+          picCaption: "home 1",
+        }),
+        Image.create({
+          position: "home",
+          awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
+          picNickname: "giphy.gif",
+          // awsPicURL,
+          picCaption: "home 1",
+        }),
+        Image.create({
+          position: "home",
+          awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
+          picNickname: "financejpg.jpg",
+          // awsPicURL,
+          picCaption: "home 2",
+        }),
+        Image.create({
+          position: "home",
+          awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
+          picNickname: "giphy.gif",
+          // awsPicURL,
+          picCaption: "home 2",
+        }),
+        Image.create({
+          position: "home",
+          awsPicID: "top-ten-matter-most-personal-finance-scaled-1-2048x1366.jpg.jpg",
+          picNickname: "financejpg.jpg",
+          // awsPicURL,
+          picCaption: "home 3",
+        }),
+        Image.create({
+          position: "home",
+          awsPicID: "finance-financial-performance-concept-illustration_53876-40450.jpg.jpg",
+          picNickname: "giphy.gif",
+          // awsPicURL,
+          picCaption: "home 3",
+        }),
+      ]);
 
     const promises = [
       samplePost1.addTags([trials, finances, money]),
