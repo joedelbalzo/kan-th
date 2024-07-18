5caefc5a69fed256c48d70fd2786dc3b7b9bcce2
joedelbalzo <joedelbalzo@users.noreply.github.com>
2024-07-18 16:55:11 +0000

    deploy: 7d3198574f6f51e9586397c29f81f6a90b284d80

---
+++ assets/BlogWrapper-ee0052f7.js
@@ -0,0 +1,1 @@
+import{j as r,O as t}from"./index-7d8d3fd7.js";const e=()=>r.jsx("div",{children:r.jsx(t,{})});export{e as default};
--- assets/OAuthHandler-e4c2ef21.js
+++
@@ -1,1 +0,0 @@
-import{T as c,a as r,b as i,r as t,Z as u,j as l}from"./index-64ac5149.js";function p(){const s=c(),a=r(),o=i(),e=new URLSearchParams(s.search).get("token"),[f,n]=t.useState(!1);return t.useEffect(()=>{e&&o(u(e)).then(()=>{n(!0),a("/portfolio")})},[e]),l.jsx("div",{children:"Success"})}export{p as default};
