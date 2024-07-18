dfd321186519352d0415dffa11d1473bd3f3e560
joedelbalzo <joedelbalzo@users.noreply.github.com>
2024-07-18 14:44:01 +0000

    deploy: db0e1c994258c820d86d9751804c01843bec6b3c

---
+++ assets/AdminWrapper-d3094dc7.js
@@ -0,0 +1,1 @@
+import{j as r,O as t}from"./index-64ac5149.js";const s=()=>r.jsx("div",{style:{marginTop:"2rem"},children:r.jsx(t,{})});export{s as default};
---
+++ assets/PortfolioWrapper-9f959db5.js
@@ -0,0 +1,1 @@
+import{c as r,a as o,r as i,j as t,O as u}from"./index-64ac5149.js";const n=()=>{const e=r(a=>a.auth),s=o();return i.useEffect(()=>{e.isNewUser&&s("/portfolio/edit")},[e,s]),t.jsx(t.Fragment,{children:t.jsx(u,{})})};export{n as default};
