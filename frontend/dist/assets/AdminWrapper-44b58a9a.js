9124ddef688905afb1651aaf64bd6a55d64877cd
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-07-18 10:36:08 -0400

    build update

---
+++ frontend/dist/assets/AdminWrapper-44b58a9a.js
@@ -0,0 +1,1 @@
+import{j as r,O as t}from"./index-eff7576b.js";const s=()=>r.jsx("div",{style:{marginTop:"2rem"},children:r.jsx(t,{})});export{s as default};
---
+++ frontend/dist/assets/BlogWrapper-a6c95824.js
@@ -0,0 +1,1 @@
+import{j as r,O as t}from"./index-eff7576b.js";const e=()=>r.jsx("div",{children:r.jsx(t,{})});export{e as default};
